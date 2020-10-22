8b6d03dfa4b30b3f14a997dfbd836b9f195f6294
itsmeow <mods.itsmeow@gmail.com>
2020-10-22 19:12:52 -0400

    Update to 1.16.3

--- src/main/java/dev/itsmeow/basemod/BaseMod.java
+++ src/main/java/dev/itsmeow/basemod/BaseMod.java
@@ -1,13 +1,8 @@
 package dev.itsmeow.basemod;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.NonNullList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
@@ -18,16 +13,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class BaseMod {
 
     public static final String MODID = "basemod";
-    private static final Logger LOGGER = LogManager.getLogger();
     public static ItemGroup ITEM_GROUP = new ItemGroup("basemod") {
         @Override
         public ItemStack createIcon() {
             return new ItemStack(Items.APPLE);
-        }
-
-        @Override
-        public void fill(NonNullList<ItemStack> toDisplay) {
-            super.fill(toDisplay);
         }
     };
 
--- src/main/java/dev/itsmeow/basemod/client/BaseModClient.java
+++ src/main/java/dev/itsmeow/basemod/client/BaseModClient.java
@@ -1,15 +1,13 @@
 package dev.itsmeow.basemod.client;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import dev.itsmeow.basemod.BaseMod;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
-@Mod.EventBusSubscriber(modid = BaseMod.MODID)
+@Mod.EventBusSubscriber(modid = BaseMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class BaseModClient {
-
-    private static final Logger LOGGER = LogManager.getLogger();
 
     @SubscribeEvent
     public static void clientSetup(final FMLClientSetupEvent event) {
